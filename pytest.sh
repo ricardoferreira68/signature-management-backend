cb32294834981eb52a4cdd8d8747fd348755d3c3
ricardoferreira68 <jrsf68@gmail.com>
2023-05-01 16:29:05 -0300

    Copy Python version from pyproject.toml

--- pytest.sh
+++ pytest.sh
@@ -1,2 +1,8 @@
 clear
+
+export PYTHON_VER=$(sed -n -e 's/^python = //p' pyproject.toml)
+echo $(echo "$PYTHON_VER" | grep -o -E '[0-9|.]+')
+export PYTHON_VER=$(echo "$PYTHON_VER" | grep -o -E '[0-9|.]+')
+sed -i "/          python-version:/c\          python-version: '$PYTHON_VER'" ./.github/workflows/pipeline.yaml 
+
 task test
